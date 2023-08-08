e5805c3288f03c46cd511bdbd4392e9d9d053e25
Jason Tang <2000jasontang@gmail.com>
2023-08-08 23:31:50 +0000

    test

--- fake.py
+++
@@ -1,9 +0,0 @@
-var1 = 1
-var2 = "2"
-var3 = {1,2,3}
-
-var4 = [var1,var2,var3]
-
-var5 = {
-"First name": "Tang"
-}
---
+++ main.py
@@ -0,0 +1,10 @@
+import module1 as mod1
+
+output1 = mod1.addition(5,10)
+output2 = mod1.subtraction(5,10)
+
+print("Output: ", output1)
+print("Output: ", output2)
+
+print(f"Output 1: {output1}")
+print(f"Output 2: {output2}")
