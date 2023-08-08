aa670647205f7c580f7388594d80befa0b74d1af
Jason Tang <2000jasontang@gmail.com>
2023-08-08 22:49:41 +0000

    updated some files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hha_509_class9
 Test repo for class 9
+
+# Example python code
+
+```
+Variable = 123
+```
---
+++ fake.py
@@ -0,0 +1,9 @@
+var1 = 1
+var2 = "2"
+var3 = {1,2,3}
+
+var4 = [var1,var2,var3]
+
+var5 = {
+"First name": "Tang"
+}
